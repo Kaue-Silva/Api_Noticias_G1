fcd5d947b8050a93991020a72b4abe329a4e9437
Kaue Silva <kauesilva940@gmail.com>
2021-09-23 18:01:23 -0300

    Api Criada

---
+++ main.py
@@ -0,0 +1,45 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+options = webdriver.ChromeOptions()
+options.headless = True
+options.add_argument("--disable-notifications")
+
+class Noticias:
+    def __init__(self):
+        self.driver = webdriver.Chrome(executable_path=r"./chromedriver.exe", options=options)
+        self.driver.get("https://g1.globo.com/")
+
+    def captura_noticias(self):
+        # Get scroll height
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+
+        while True:
+            # Botão scroll para baixo
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+            # esperar a pagina carregar
+            sleep(0.5)
+
+            # calcular novo scroll e comparar com scroll total da pagina
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            
+            last_height = new_height
+        
+        # pagina = self.driver.find_element_by_xpath('//body')
+        # pagina.send_keys(Keys.END)
+        
+        # sleep(5)
+        
+        resumo_noticias = {}
+        noticias = self.driver.find_elements_by_xpath('//div [@class="feed-post-body-resumo"]')
+        for n in range(len(noticias)):
+            resumo_noticias["id":n] = f"{n+1}º Noticia: {noticias[n].text}"
+        
+        return resumo_noticias
+
+    def sair(self):
+        self.driver.close()
---
+++ start.py
@@ -0,0 +1,32 @@
+from main import Noticias
+from flask import Flask, jsonify
+from flask_restx import Api, Resource, namespace
+
+class Server():
+    def __init__(self):
+        self.app = Flask(__name__)
+        self.api = Api(self.app, 
+        version=1.0,
+        title='API G1 Noticias',
+        description='Api que retorna resumo das principais noticias do site G1.',
+        doc='/'
+        )
+    
+    def run(self):
+        self.app.run()
+
+server = Server()
+
+app, api = server.app, server.api
+
+@api.route('/noticias')
+
+class resumo_noticias(Resource):
+    def get(self):
+        noticias = Noticias()
+        noticias_dados = noticias.captura_noticias()
+        noticias.sair()
+        return noticias_dados
+
+if __name__ == "__main__":
+    server.run()
