5a8311f14b6c465f435203d2104581f0e51e16c6
kauesilva <kauesilva940@gmail.com>
2022-03-26 21:20:26 -0300

    Imagem default e Reformulando selenium

--- app/views/noticias.py
+++ app/views/noticias.py
@@ -8,8 +8,9 @@ class resumo_noticias(Resource):
     def get(self):
         noticias = Noticias()
         noticias.carregamento_pagina()
-        noticias.noticias_texto()
-        noticias.noticias_imagens()
+        noticias.noticias_titulos()
+        # noticias.noticias_texto()
+        # noticias.noticias_imagens()
         noticias_dados = noticias.noticias_dados()
         noticias.sair()
         return noticias_dados
