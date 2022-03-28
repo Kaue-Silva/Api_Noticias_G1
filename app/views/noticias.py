d023e094af980251ecc93bf9af416390c174617c
kauesilva <kauesilva940@gmail.com>
2022-03-28 18:58:00 -0300

    Implementando Views

--- app/views/noticias.py
+++ app/views/noticias.py
@@ -6,14 +6,24 @@ from ..controllers.noticias import Noticias
 @api.route('/noticias')
 class resumo_noticias(Resource):
     def get(self):
-        noticias = Noticias()
-        noticias.carregamento_pagina()
-        noticias.get_titulo()
+        # noticias = Noticias()
+        # noticias.carregamento_pagina()
+        # noticias.get_titulo()
         # noticias.noticias_texto()
         # noticias.noticias_titulos()
         # noticias.noticias_complementar()
         # noticias.noticias_texto()
         # noticias.noticias_imagens()
-        noticias_dados = noticias.noticias_dados()
+        try:
+            noticias = Noticias()
+            noticias.carregamento_pagina()
+            noticias.get_titulos()
+            noticias.get_complementos()
+            noticias.get_hora_local()
+            noticias.get_imagem()
+            noticias_dados = (noticias.noticias_dados(), 200)
+        except:
+            noticias_dados = ({"status":"ocorreu um erro inesperado"}, 502)
+        
         noticias.sair()
         return noticias_dados
